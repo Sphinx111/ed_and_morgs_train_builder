9fa174e8c46d94d05207fe22fddad4a8fafe0041
MorgueXiiV <3975514+MorgueXIIV@users.noreply.github.com>
2025-07-29 16:38:21 +0100

    prorgess on buttons to add a new module to cars gui-ily

--- Scenes/construction_panel.gd
+++ Scenes/construction_panel.gd
@@ -6,13 +6,17 @@ var buttons : Array [Button] = []
 
 func setup():
 	var newButton = Button.new()
-	newButton.text="amaazign"
-	add_child(newButton)
-	buttons.append(newButton)
-	newButton.text="%d" % buttons.size()
-	newButton.position.x = buttons.size() * 50 + 10
-	newButton.pressed.connect(pressButton.bind(newButton.text))
+	var buttonList = ["water", "food", "kitchen", "cabin"]
+	for buttonWord in buttonList:
+		newButton = Button.new()
+		newButton.text=buttonWord
+		add_child(newButton)
+		buttons.append(newButton)
+		newButton.position.x = buttons.size() * 60 + 10
+		newButton.pressed.connect(pressButton.bind(newButton.text))
 	
 func pressButton(textyargument):
-	print ("%s amazingnesses achieved" % textyargument)
+	# $LineEdit.text = 
+	print ("add %s at" % textyargument)
+#	$selectCarPanel.show()
 	
