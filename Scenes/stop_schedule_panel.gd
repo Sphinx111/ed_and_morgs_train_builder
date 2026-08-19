67443317c13a69cb7bf08c06c8f029f5beb34024
morgueXiiV <3975514+MorgueXIIV@users.noreply.github.com>
2026-08-19 20:01:05 +0100

    added function to stop scheduler buttons - printing resource distance on console

--- Scenes/stop_schedule_panel.gd
+++ Scenes/stop_schedule_panel.gd
@@ -3,7 +3,7 @@ extends Panel
 class_name StopSchedulePanel
 
 var buttons : Array [Button]
-var buttonList : Array [String] = ["water", "oil", "people"]
+var buttonList : Array [String] = ["grey_water", "clean_water", "oil", "pop", "scrap", "mech_parts","food1"]
 
 func setup() : 
 	var i=0
@@ -11,7 +11,13 @@ func setup() :
 		var newButton : Button = Button.new() 
 		add_child(newButton)
 		newButton.text = nameb
-		newButton.position = Vector2(0,i*50)
+		newButton.position = Vector2(0,i*35)
+		newButton.pressed.connect(resourceSelectPress.bind(newButton.text))
 		buttons.append(newButton)
 		i+=1
 	
+
+func resourceSelectPress(resourceSelection) : 
+	var ourMap : MapHandler = get_parent().worldMap
+	#var nextResource : MapDestination = ourMap.get_next_resource_spot(resourceSelection)
+	print("Next %s Well is at %f" % [resourceSelection, ourMap.get_distance_to_next_resource(resourceSelection)])	
